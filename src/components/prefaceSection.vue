65e97c3926bd03c5116d672249d02b84c0289555
David Wismer <david.wismer@mindmaze.ch>
2025-05-18 14:26:33 +0200

    manage text wrap in preface

--- src/components/prefaceSection.vue
+++ src/components/prefaceSection.vue
@@ -11,7 +11,7 @@
                 cercueil. Une sorte de béret gavroche à carreaux rouges et noirs. Il fut incinéré avec cette casquette.
                 Cet objet qui le décrivait si bien. Je me souvient alors m'être demandé: « Pourquoi mettre cette
                 casquette avec mon grand-père, si c'est pour la brûler en même temps que lui ? Pourquoi ne pas avoir
-                garder cet objet comme souvenir ? »
+                garder cet objet comme souvenir&nbsp;? »
             </p>
             <p>
                 Depuis petite, la mort me hante par intermittence, comme un rappel du vide sous les choses. J’ai cherché
@@ -19,54 +19,57 @@
                 que reste-t-il de nous une fois disparus ? La mort s’infiltre comme un courant d’air glacé. Elle entre
                 et souffle des questions que je n’ai pas invitées. Pourquoi avons-nous si peur de disparaître ? Pourquoi
                 l’idée qu’il ne reste rien — ni trace, ni image, ni souvenir — nous ( ou en tout cas me ) semble-t-elle
-                insupportable ? <br>
+                insupportable&nbsp;? <br>
                 Plutôt que fuir cette angoisse, j’ai voulu la transformer. La regarder. En faire quelque chose. Ce
                 projet est né d’un vertige intime, mais il est devenu collectif : il est traversé par des voix, des
-                objets, des désirs de survivre. Une tentative douce de parler de la mort autrement.
+                objets, des désirs de survivre. Une tentative douce de parler de la mort&nbsp;autrement.
             </p>
             <p>
                 Nos visages persistent sur les réseaux. Nos messages s’accumulent. Et pourtant, plus que jamais, nous
                 avons peur de disparaître. Alors, nous fabriquons des doubles, des masques, des reliques. Notre époque
                 numérise tout, même le souvenir. Nous scannons les visages, archivons nos pensées,faisons vivre nos
                 morts à travers des intelligences artificielles. Le deuil est connecté, dématérialisé, parfois même
-                monétisé. Mais sous cette avalanche de données, il reste une obsession ancienne : ne pas être oublié.
+                monétisé. Mais sous cette avalanche de données, il reste une obsession ancienne : ne pas
+                être&nbsp;oublié.
                 <br>
                 Les objets prennent aujourd’hui une place ambivalente. Ils sont à la fois témoins, extensions,
                 talismans, masques. Une lettre, une photo, une vieille peluche peuvent porter le poids d’un être entier.
                 Quand les rites funéraires traditionnels s’effacent, ces objets deviennent nos tombeaux portables. Nos
-                mémoriaux intimes. Nos cris silencieux dans l’éternité numérique.
+                mémoriaux intimes. Nos cris silencieux dans l’éternité&nbsp;numérique.
             </p>
             <p>
                 Un objet devient ce que l’on y projette ( une présence, un espoir, une promesse de retour.) Il fixe
                 quelque chose de nous au-delà du corps. J’ai demandé à des personnes ce qu’elles emporteraient dans la
                 mort, et par quel objet on se souviendrait d’elles. Certain·es ont choisi un bijou, une photo, un
                 dessin. D’autres ont évoqué leurs habits, leur disque dur, ou leur parfum. Il y a des objets d’amour,
-                des objets de révolte, des objets absurdes et magnifiques. <br>
+                des objets de révolte, des objets absurdes et&nbsp;magnifiques. <br>
                 Chaque objet devient un point d’ancrage contre l’oubli. Il contient une part de l’identité, de
                 l’histoire, de l’essence de la personne. Ils sont aussi des résistances symboliques : face au silence de
-                la mort, ils parlent. Face à la disparition, ils persistent. <br>
+                la mort, ils parlent. Face à la disparition, ils&nbsp;persistent. <br>
                 Chercher à survivre dans l’objet, c’est peut-être aussi refuser que tout s’éteigne. Mais c’est aussi
-                admettre qu’un reste suffit. Qu’un reste peut dire tout. Mais à quoi bon vouloir être encore là, après ?
+                admettre qu’un reste suffit. Qu’un reste peut dire tout. Mais à quoi bon vouloir être encore là,
+                après&nbsp;?
                 <br>
-                Est-ce que ce désir de mémoire est une preuve d’amour... ou une forme d’arrogance ? <br>
-                Une tentative de contrôler ce qui, par essence, nous échappe ? <br>
+                Est-ce que ce désir de mémoire est une preuve d’amour... ou une forme d’arrogance&nbsp;? <br>
+                Une tentative de contrôler ce qui, par essence, nous échappe&nbsp;? <br>
                 Je me demande souvent si l’objet posthume est un cri ou une caresse. Un monument égoïste ou une
-                offrande. <br>
+                &nbsp;offrande. <br>
                 Peut-être qu’il est les deux. Peut-être qu’on cherche autant à consoler ceux qui restent qu’à se
                 rassurer soi-même. <br>
-                L’objet devient un entre-deux : ni vivant, ni mort. Un morceau de nous qui n’a pas tout à fait disparu.
+                L’objet devient un entre-deux : ni vivant, ni mort. Un morceau de nous qui n’a pas tout à
+                fait&nbsp;disparu.
             </p>
             <p>
                 Si je ne devais emporter qu’un objet, je ne sais pas encore lequel je choisirais. Mais je sais que je
-                veux qu’il parle de moi sans me contenir tout entière. Juste une étincelle. Un fragment. Un morceau de
-                silence. <br>
+                veux qu’il parle de moi sans me contenir tout entière. Juste une étincelle. Un fragment. Un morceau
+                de&nbsp;silence. <br>
                 Ce projet n’apporte pas de réponse définitive. Il ouvre un espace. Il fait de la mort une conversation
-                plutôt qu’un tabou.
+                plutôt qu’un&nbsp;tabou.
             </p>
             <p style="text-align: center;">
-                Peut-être que c’est ça, la mémoire. <br>
-                Une trace assez faible pour être oubliée. <br>
-                Assez forte pour revenir.
+                Peut-être que c’est ça, la&nbsp;mémoire. <br>
+                Une trace assez faible pour être&nbsp;oubliée. <br>
+                Assez forte pour&nbsp;revenir.
             </p>
         </div>
     </section>
@@ -77,7 +80,7 @@ p {
     font-size: 9pt;
     color: rgba(0, 0, 0, .3);
     margin-bottom: 20px;
-    text-wrap: pretty;
     margin-top: 15px;
+    text-wrap: pretty;
 }
 </style>
